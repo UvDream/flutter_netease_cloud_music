ebd6bcca6b9a9bceb30d05c0736f6cd5aacd6ae6
wangzhongjie <uvdream@163.com>
2019-08-05 17:52:43 +0800

    [fix] 没完成的本地音乐提交

---
+++ lib/pages/my/center_block.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class CenterBlockPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: <Widget>[_block(0xe601, '本地音乐', 0)],
+      ),
+    );
+  }
+
+  Widget _block(icon, title, num) {
+    return Container(
+      child: Row(
+        children: <Widget>[],
+      ),
+    );
+  }
+}
--- lib/pages/my/my.dart
+++ lib/pages/my/my.dart
@@ -2,13 +2,14 @@
  * @Author: wangzhongjie
  * @Date: 2019-02-20 15:47:58
  * @LastEditors: wangzhongjie
- * @LastEditTime: 2019-08-05 14:22:14
+ * @LastEditTime: 2019-08-05 17:49:33
  * @Description: 我的
  * @Email: uvdream@163.com
  */
 import 'package:flutter/material.dart';
 import '../../components/play.dart';
 import './top_area.dart';
+import './center_block.dart';
 
 class MyPage extends StatelessWidget {
   @override
@@ -32,9 +33,7 @@ class MyPage extends StatelessWidget {
         ),
         body: SingleChildScrollView(
             child: Column(
-          children: <Widget>[
-            TopAreaPage(),
-          ],
+          children: <Widget>[TopAreaPage(), CenterBlockPage()],
         )));
   }
 }
