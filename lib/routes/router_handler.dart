0a7186568943ac40282a10ce003eacd9554ab65e
wangzhongjie <uvdream@163.com>
2019-08-08 13:42:11 +0800

    [feat] 路由

--- lib/routes/router_handler.dart
+++ lib/routes/router_handler.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:fluro/fluro.dart';
 import '../pages/song_list/song_list.dart';
 
-Handler detailhandler = Handler(
+Handler songListHandler = Handler(
     handlerFunc: (BuildContext context, Map<String, List<String>> params) {
   String songListId = params['id'].first;
   print('路由参数${songListId}');
--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -4,12 +4,12 @@ import './router_handler.dart';
 
 class Routes {
   static String root = '/';
-  static String detailsPage = '/detail';
+  static String songListPage = '/songList';
   static void configureRoutes(Router router) {
     router.notFoundHandler = new Handler(
         handlerFunc: (BuildContext context, Map<String, List<String>> params) {
       print('ERROR=====>ROUTE NOT FOUND');
     });
-    router.define(detailsPage, handler: detailhandler);
+    router.define(songListPage, handler: songListHandler);
   }
 }
