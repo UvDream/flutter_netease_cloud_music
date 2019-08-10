626c1d209a3459c799a59144a44f769de9e53dce
wangzhongjie <uvdream@163.com>
2019-08-10 19:26:14 +0800

    [fix] 列表

--- lib/pages/song_list/song_list.dart
+++ lib/pages/song_list/song_list.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:ui';
 
 class SongListPage extends StatelessWidget {
   final String songListId;
@@ -6,17 +7,66 @@ class SongListPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('列表页面'),
-          leading: IconButton(
-            icon: Icon(Icons.arrow_back_ios),
-            onPressed: () {
-              Navigator.pop(context);
-            },
-          ),
+      body: NestedScrollView(
+        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+          return <Widget>[
+            SliverOverlapAbsorber(
+              handle: NestedScrollView.sliverOverlapAbsorberHandleFor(context),
+              child: SliverAppBar(
+                leading: Icon(Icons.arrow_back_ios),
+                title: Text('歌单'),
+                expandedHeight: 200.0,
+                pinned: true,
+                centerTitle: true,
+                flexibleSpace: FlexibleSpaceBar(
+                  centerTitle: true,
+                  title: Container(
+                    color: Colors.red,
+                    height: 50,
+                    child: Text('收藏歌单'),
+                  ),
+                  background: Stack(
+                    children: <Widget>[
+                      ConstrainedBox(
+                        constraints: const BoxConstraints.expand(),
+                        child: Image.network(
+                          'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1545738629147&di=22e12a65bbc6c4123ae5596e24dbc5d3&imgtype=0&src=http%3A%2F%2Fpic30.photophoto.cn%2F20140309%2F0034034413812339_b.jpg',
+                          fit: BoxFit.fitWidth,
+                        ),
+                      ),
+                      Positioned(
+                        bottom: 0,
+                        child: Container(
+                          child: BackdropFilter(
+                            filter: ImageFilter.blur(sigmaX: 5.0, sigmaY: 5.0),
+                            child: Opacity(
+                              opacity: .5,
+                              child: Container(
+                                child: Text('测试'),
+                              ),
+                            ),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            )
+          ];
+        },
+        body: ListView.builder(
+          itemBuilder: _itemBuilder,
+          itemCount: 15,
         ),
-        body: Center(
-          child: Text(songListId),
-        ));
+      ),
+    );
+  }
+
+  Widget _itemBuilder(BuildContext context, int index) {
+    return ListTile(
+      leading: Icon(Icons.android),
+      title: Text('无与伦比的标题+$index'),
+    );
   }
 }
