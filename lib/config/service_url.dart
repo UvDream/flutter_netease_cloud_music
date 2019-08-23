6395a96fffb3ea5ea1855d33149a2c1dc4c1fa19
wangzhongjie <uvdream@163.com>
2019-08-23 17:10:00 +0800

    [feat] 歌曲mv详情

--- lib/config/service_url.dart
+++ lib/config/service_url.dart
@@ -3,5 +3,6 @@ const servicePath = {
   'findSwiper': serviceUrl + '/banner', //首页轮播图
   'songListApi': serviceUrl + '/personalized', //推荐歌单
   'newDishApi': serviceUrl + '/top/album', //新碟
-  'songListDetail': serviceUrl + '/playlist/detail' //歌单详情
+  'songListDetail': serviceUrl + '/playlist/detail', //歌单详情
+  'mvDetail': serviceUrl + '/mv/detail' //歌曲mv详情
 };
