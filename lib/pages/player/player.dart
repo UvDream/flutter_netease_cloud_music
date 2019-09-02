fd02cbbfd51e55cfc8385f9915b03b9a66d9fecf
wangzhongjie <uvdream@163.com>
2019-09-02 09:24:52 +0800

    [feat]播放界面

---
+++ lib/pages/player/player.dart
@@ -0,0 +1,23 @@
+/*
+ * @Author: wangzhongjie
+ * @Date: 2019-09-02 09:23:21
+ * @LastEditors: wangzhongjie
+ * @LastEditTime: 2019-09-02 09:23:21
+ * @Description: 播放界面
+ * @Email: UvDream@163.com
+ */
+import 'package:flutter/material.dart';
+
+class PlayerPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('播放界面'),
+      ),
+      body: Container(
+        child: Text('播放界面'),
+      ),
+    );
+  }
+}
