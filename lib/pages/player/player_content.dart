7fd887329bf5e039ce5eb1059540a5fe3d3b74df
wangzhongjie <UvDream@163.com>
2019-09-05 15:06:10 +0800

    [feat] :tada: 播放界面基本完成

--- lib/pages/player/player_content.dart
+++ lib/pages/player/player_content.dart
@@ -2,7 +2,7 @@
  * @Author: wangzhongjie
  * @Date: 2019-09-03 16:19:55
  * @LastEditors: wangzhongjie
- * @LastEditTime: 2019-09-05 14:02:52
+ * @LastEditTime: 2019-09-05 15:05:48
  * @Description: 播放器中间区域
  * @Email: UvDream@163.com
  */
@@ -13,9 +13,8 @@ class PlayerContent extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.white,
       width: ScreenUtil().setWidth(750),
-      height: ScreenUtil().setHeight(800),
+      height: ScreenUtil().setHeight(850),
       child: Text('歌词区域'),
     );
   }
