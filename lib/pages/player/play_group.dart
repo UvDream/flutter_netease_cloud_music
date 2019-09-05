c996e1f05351edad31d00642a44aeb408a04e487
wangzhongjie <UvDream@163.com>
2019-09-05 14:53:19 +0800

    [feat] :tada: 播放组界面播放按钮

--- lib/pages/player/play_group.dart
+++ lib/pages/player/play_group.dart
@@ -2,17 +2,38 @@
  * @Author: wangzhongjie
  * @Date: 2019-09-03 16:24:50
  * @LastEditors: wangzhongjie
- * @LastEditTime: 2019-09-03 16:30:47
+ * @LastEditTime: 2019-09-05 14:52:44
  * @Description: 播放按钮区域
  * @Email: UvDream@163.com
  */
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class PlayGroup extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text("播放"),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          _btn(0xe667, 30.0),
+          _btn(0xe664, 30.0),
+          _btn(0xe666, 50.0),
+          _btn(0xe663, 30.0),
+          _btn(0xe66a, 30.0),
+        ],
+      ),
+    );
+  }
+
+  Widget _btn(icon, size) {
+    return Container(
+      width: ScreenUtil().setWidth(140),
+      child: Icon(
+        IconData(icon, fontFamily: "IconFont"),
+        color: Colors.white,
+        size: size,
+      ),
     );
   }
 }
